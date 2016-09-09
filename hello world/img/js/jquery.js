fabc08acb4330c2f8f8d9801bdba8535a429a35f
Rebecca <speir047@newschool.edu>
2016-09-09 18:51:50 +0000

    test

---
+++ hello world/img/js/jquery.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('#content').delay(8000).fadeIn(400);
+});
---
+++ project 2/js/script.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+console.log("another line");
+
+var student = "Brendan";
+var school = "Parsons";
+
+if (student == "Brendan" && school == "Parsons"){console.log("come on in");
+
+}
