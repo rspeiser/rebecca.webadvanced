dce81556a5ab22e9038a4433d1d2bb0ca22416de
Rebecca <speir047@newschool.edu>
2016-12-15 21:33:31 +0000

    update

--- api/js/jquery.js
+++ api/js/jquery.js
@@ -1,11 +1,27 @@
-$(function(){
-
-var hexagon =100;
-    for(var x=1;x<=hexagon;x++){
-    var posx = Math.round(Math.random() * 1920)-20;
-    var posy = Math.round(Math.random() * 1080)-20;
-    var ticket="<div class='.hexagon' style='position:absolute;left:'+posy+';top:'+posx+';'><p>Random<br />Box</p></div>";
-    $(ticket).appendTo("body");
+<script type="text/javascript">/* <![CDATA[ */
+if (top != self) {
+    try {
+        if (parent != top) {
+            throw 1;
+        }
+        var disallowed = ['XXXXXXX.com'];
+        var href = top.location.href.toLowerCase();
+        for (var i = 0; i < disallowed.length; i++) {
+            if (href.indexOf(disallowed[i]) >= 0) {
+                throw 1;
+            }
+        }
+    } catch (e) {
+        try {
+            window.document.getElementsByTagName('head')[0].innerHTML = '';
+        } catch (e) { /* IE */
+            var htmlEl = document.getElementsByTagName('html')[0];
+            htmlEl.removeChild(document.getElementsByTagName('head')[0]);
+            var el = document.createElement('head');
+            htmlEl.appendChild(el);
+        }
+        window.document.body.innerHTML = '<a href="#" onclick="top.location.href=window.location.href" style="text-decoration:none;"><img src="http://www.XXXXXXX.com/img/XXXXXX.gif" style="border:0px;" /><br />Go to XXXXXXX.com</a>';
     }
+}
 
-});
+/* ]]> */</script>
